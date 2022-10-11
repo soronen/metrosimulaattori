8843fa4cde32edf11618aa5f863bb65f7e6b9b15
Eetu Soronen <eetu.soronen@hotmail.com>
2022-10-11 10:38:38 +0300

    TapahtumanTyyppi javadoc

--- metrosimulaattori/src/application/simu/model/TapahtumanTyyppi.java
+++ metrosimulaattori/src/application/simu/model/TapahtumanTyyppi.java
@@ -2,6 +2,13 @@ package application.simu.model;
 
 // TODO:
 // Tapahtumien tyypit määritellään simulointimallin vaatimusten perusteella
+
+/**
+ * Nämö enum-arvot toimivat erottavat palvelupisteet ja saapumisen toisistaan
+ *
+ * @author Eetu Soronen
+ * @version 1
+ */
 public enum TapahtumanTyyppi {
 	ARRIVAL,
 	ENTRANCE,
