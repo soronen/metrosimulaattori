7e879be1de566e791eebe55ddb7e43028a0d2701
Eetu Soronen <eetu.soronen@hotmail.com>
2022-10-11 13:53:29 +0300

    tietoa meistä popup

--- metrosimulaattori/src/application/view/RootLayoutController.java
+++ metrosimulaattori/src/application/view/RootLayoutController.java
@@ -3,6 +3,12 @@ package application.view;
 import application.MainApp;
 import application.controller.Kontrolleri;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class RootLayoutController {
 
@@ -36,5 +42,22 @@ public class RootLayoutController {
 
     @FXML
     public void avaagraphview(){mainApp.showgraphview();}
+
+    @FXML
+    public void aboutUs() {
+        final Stage dialog = new Stage();
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.initOwner(this.mainApp.getPrimaryStage());
+        VBox dialogVbox = new VBox(20);
+        Text t = new Text("  Tekijät: Eetu Soronen ja Emil Ålgars  ");
+        Text t2 = new Text("  metrosimulaattori🚇(tm) 2022 all rights reserved  ");
+        t.setStyle("-fx-font: 16 arial;");
+        t2.setStyle("-fx-font: 20 'Old English Text MT'");
+        dialogVbox.getChildren().add(t);
+        dialogVbox.getChildren().add(t2);
+        Scene dialogScene = new Scene(dialogVbox, 420, 80);
+        dialog.setScene(dialogScene);
+        dialog.show();
+    }
 
 }
